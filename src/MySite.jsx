dadbc5349388f42fbbab136f9e76f33ca8e6957a
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-11 13:32:20 +0100

    wip

--- src/MySite.jsx
+++ src/MySite.jsx
@@ -15,34 +15,20 @@ const sections = [
   { heading: 'Section 3', content: 'content 3' },
 ];
 
-class MySite extends React.Component {
-  // 1- implement a constructor and initialize state to contain links and sections
-  constructor(props) {
-    super(props);
-    this.state = {
-      links,
-      sections,
-    };
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <div className='container'>
-        <header>
-          <h1>My Website</h1>
-          <Nav links={this.state.links} />
-        </header>
-        <div className='sections-container'>
-          {
-            this.state.sections.map(section => <Section section={section} />)
-          }
-        </div>
+function MySite() {
+  return (
+    <div className='container'>
+      <header>
+        <h1>My Website</h1>
+        <Nav links={links} />
+      </header>
+      <div>
+        {
+          sections.map(section => <Section key={section.heading} section={section} />)
+        }
       </div>
-    );
-  }
-  // 2- implement a simplistic render method that returns Hello World
-  // 5- implement a render method that returns the nav and the sections
+    </div>
+  );
 }
 
 // 3- implement a functional component 'Nav' that takes links and returns a nav
@@ -50,18 +36,19 @@ function Nav({ links }) {
   return (
     <nav className='my-nav'>
       {
-        links.map(link => <a href={link.url}>{link.label}</a>)
+        links.map(link => <a key={link.label} href={link.url}>{link.label}</a>)
       }
     </nav>
   );
 }
+
 // 4- implement a functional component 'Section' that takes { heading, content }
 function Section({ section }) {
   return (
-    <div className='section'>
+    <section>
       <h3>{section.heading}</h3>
       <p>{section.content}</p>
-    </div>
+    </section>
   );
 }
 // and returns a section with an h3 heading and a paragraph.
